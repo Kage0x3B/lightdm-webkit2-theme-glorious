c28350a6b5ad66bf844d2382466433e31aaaecd6
Moritz Hein <moritz.hein@live.de>
2022-05-13 15:32:30 +0200

    Format code

--- js/script.js
+++ js/script.js
@@ -1,86 +1,86 @@
 let ran = false;
 
 async function initGreeter() {
-  if (ran) return;
-  ran = true;
+    if (ran) return;
+    ran = true;
 
-  if (greeter_config.greeter.debug_mode) {
-    // Instantiate debug mode
-    // Comment this line to let lightdm do its own things
-    debug = new Debug();
-  }
+    if (greeter_config.greeter.debug_mode) {
+        // Instantiate debug mode
+        // Comment this line to let lightdm do its own things
+        debug = new Debug();
+    }
 
-	lightdm.authentication_complete?.connect( () => authentication_complete() );
+    lightdm.authentication_complete?.connect(() => authentication_complete());
 
-  lightdm.show_prompt?.connect( (prompt, type) => {
-    console.log("PROMPT!");
-    console.log(prompt, type);
-  } );
+    lightdm.show_prompt?.connect((prompt, type) => {
+        console.log("PROMPT!");
+        console.log(prompt, type);
+    });
 
-  lightdm.show_message?.connect( (msg, type) => {
-    console.log("MESSAGE!");
-    console.log(msg, type);
-  } );
+    lightdm.show_message?.connect((msg, type) => {
+        console.log("MESSAGE!");
+        console.log(msg, type);
+    });
 
-  // Instantiate image profile
-  userProfile = new UserProfile();
+    // Instantiate image profile
+    userProfile = new UserProfile();
 
-  // Instantiate greeter screen
-  greeterScreen = new GreeterScreen();
+    // Instantiate greeter screen
+    greeterScreen = new GreeterScreen();
 
-  // Instantiate dark screen
-  loginFade = new LoginFade();
+    // Instantiate dark screen
+    loginFade = new LoginFade();
 
-  // Instantiate goodbye screen
-  goodbye = new Goodbye();
+    // Instantiate goodbye screen
+    goodbye = new Goodbye();
 
-  // Instantiate accounts settings
-  accounts = new Accounts();
+    // Instantiate accounts settings
+    accounts = new Accounts();
 
-  // Instantiate sessions settings
-  sessions = new Sessions();
+    // Instantiate sessions settings
+    sessions = new Sessions();
 
-  // Instantiate power settings
-  power = new Power();
+    // Instantiate power settings
+    power = new Power();
 
-  // Instantiate sidebar
-  sidebar = new Sidebar();
+    // Instantiate sidebar
+    sidebar = new Sidebar();
 
-  // Instantiate main screen buttons
-  mainScreenButtons = new MainScreenButtons();
+    // Instantiate main screen buttons
+    mainScreenButtons = new MainScreenButtons();
 
-  // Instantiate date time
-  dateTime = new DateTime();
+    // Instantiate date time
+    dateTime = new DateTime();
 
-  // Instantiate backgrounds settings
-  backgrounds = new Backgrounds();
-  await backgrounds._init();
+    // Instantiate backgrounds settings
+    backgrounds = new Backgrounds();
+    await backgrounds._init();
 
-  // Instantiate sidebar navigation
-  sidebarNavigate = new SidebarNavigate();
+    // Instantiate sidebar navigation
+    sidebarNavigate = new SidebarNavigate();
 
-  // Instantiate key binds
-  keyBinds = new KeyBinds();
+    // Instantiate key binds
+    keyBinds = new KeyBinds();
 
-  // Instantiate themes
-  themes = new Themes();
+    // Instantiate themes
+    themes = new Themes();
 
-  // Instantiate key events
-  keyEvents = new KeyEvents();
+    // Instantiate key events
+    keyEvents = new KeyEvents();
 
-  // Instantiate swipe event callbacks
-  swipeEventCallback = new SwipeEventCallback();
+    // Instantiate swipe event callbacks
+    swipeEventCallback = new SwipeEventCallback();
 
-  // Instantiate authentication
-  authenticate = new Authenticate();
+    // Instantiate authentication
+    authenticate = new Authenticate();
 
- }
+}
 
 window.addEventListener("GreeterReady", initGreeter)
 
 const greeterReady = new Event("GreeterReady");
 
 setTimeout(() => {
-  if (!("lightdm" in window)) debug = new Debug();
-  window.dispatchEvent(greeterReady);
+    if (!("lightdm" in window)) debug = new Debug();
+    window.dispatchEvent(greeterReady);
 }, 1000)
